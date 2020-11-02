d410e884977d3d36d31c21b5ef631186bfb892d8
Alexandrov Vitaliy <aleksandrov.vetall@yandex.ru>
2020-11-02 21:03:42 +0300

    Updates

--- precache-manifest.d00228ac68722cb132a989a60da168eb.js
+++ precache-manifest.d00228ac68722cb132a989a60da168eb.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f0761e0c12ba1fbb328b178eecd5d660",
+    "revision": "11e799d644403b0fbca84246978772d8",
     "url": "/vk-mini-app-spb-archive-search/index.html"
   },
   {
-    "revision": "7bab562a07b69e79bc83",
+    "revision": "0e0267794f79056c6b7e",
     "url": "/vk-mini-app-spb-archive-search/static/css/2.6326594c.chunk.css"
   },
   {
-    "revision": "6588694892663db6d88a",
+    "revision": "1e6be99c9b6acd0af8e1",
     "url": "/vk-mini-app-spb-archive-search/static/css/main.c61021fa.chunk.css"
   },
   {
-    "revision": "7bab562a07b69e79bc83",
-    "url": "/vk-mini-app-spb-archive-search/static/js/2.bc5807e8.chunk.js"
+    "revision": "0e0267794f79056c6b7e",
+    "url": "/vk-mini-app-spb-archive-search/static/js/2.8c337798.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "/vk-mini-app-spb-archive-search/static/js/2.bc5807e8.chunk.js.LICENSE.txt"
+    "url": "/vk-mini-app-spb-archive-search/static/js/2.8c337798.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6588694892663db6d88a",
-    "url": "/vk-mini-app-spb-archive-search/static/js/main.941535a1.chunk.js"
+    "revision": "1e6be99c9b6acd0af8e1",
+    "url": "/vk-mini-app-spb-archive-search/static/js/main.5209d9bc.chunk.js"
   },
   {
     "revision": "fb9ad18f58c61d4a802f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-mini-app-spb-archive-search/precache-manifest.49f049800c53566c69861a96d36d0bdc.js"
+  "/vk-mini-app-spb-archive-search/precache-manifest.d00228ac68722cb132a989a60da168eb.js"
 );
 
 self.addEventListener('message', (event) => {
