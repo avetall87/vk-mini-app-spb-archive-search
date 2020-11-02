e47b678a69c4982444a420c4db2654f1eb27da9c
avorontsov <vorontsov@iac.spb.ru>
2020-11-02 10:45:40 +0300

    New style

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -2,61 +2,75 @@ import React, {useEffect, useState} from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
-import Input from "@vkontakte/vkui/dist/components/Input/Input";
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import PanelHeaderContent from '@vkontakte/vkui/dist/components/PanelHeaderContent/PanelHeaderContent';
+import Search from '@vkontakte/vkui/dist/components/Search/Search';
+
+import './Home.css';
 
 
 const Home = ({id, go, fetchedUser}) => {
 
-    const [getName, setName] = useState('');
-    const [getSearchParams, setSearchParams] = useState('');
+	const [getName, setName] = useState('');
+	const [getHumanName, setHumanName] = useState('');
 
-    useEffect(() => {
-        reset();
-    }, []);
+	useEffect(() => {
+		//setName('');
+		reset();
+	}, []);
 
-    const search = async () => {
-        window.open(`https://medal.spbarchives.ru/search?query=${getSearchParams}&advancedSearch=false`);
-    }
+	const search = async () => {
+		window.open(`https://medal.spbarchives.ru/search?query=${getHumanName}&advancedSearch=false&from=vk`);
+	}
 
-    const reset = () => {
-        setName('');
-        setSearchParams('');
-    }
+	const _handleKeyDown = (e) => {
+		if (e.key === 'Enter') {
+			window.open(`https://medal.spbarchives.ru/search?query=${getHumanName}&advancedSearch=false&from=vk`);
+		}
+	}
 
-    const onLabelChange = (event) => {
-        setSearchParams(event.target.value);
-    };
+	const reset = () => {
+		setName('');
+		setHumanName('');
+	}
 
-    return (<Panel id={id}>
-            {/*<PanelHeader>Поиск архивных документов</PanelHeader>*/}
-            {fetchedUser &&
-            <Group title="Ползователь">
-                <Cell
-                    before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-                    description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-                >
-                    {`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-                </Cell>
-            </Group>}
+	const onLabelChange = (event) => {
+		setHumanName(event.target.value);
+	};
 
-            <Group>
-                <Div>
-                    <h2 align="center">Поиск награжденных медалью</h2>
-                    <Input id="medalSearchId" type="text" value={getSearchParams} onChange={onLabelChange} placeholder="ФИО, год рождения, место работы"/>
-                    {/*<br/>*/}
-                    {/*<br/>*/}
-                    <div style={{display: "flex", padding: "10px", margin: "10px", justifyContent: "space-between"}}>
-                        <Button size="xl" title="Искать на сайте: Медаль ЗА ОБОРОНУ ЛЕНИИНГРАДА" onClick={search}>
-                            Искать
-                        </Button>
-                    </div>
-                </Div>
-            </Group>
-        </Panel>
-    )
+	return (<Panel id={id}>
+			<PanelHeader>
+
+				<PanelHeaderContent
+					// status="В базу внесены данные на 167 785 персоналий. Работа продолжается"
+					before={<Avatar size={36} src="https://spbarchives.ru/documents/10157/1507999/medal_150_150.png" />}
+				>
+					<span class="PageHeaderContent"> Медаль «За оборону Ленинграда»</span>
+				</PanelHeaderContent>
+				{/*Медаль «За оборону Ленинграда»*/}
+			</PanelHeader>
+			<Group>
+				<Div>
+					<h3 align="left" style={{ paddingLeft: 16, fontWeight: 500}} >Поиск награжденных медалью</h3>
+					<Search
+						placeholder="ФИО, год рождения, место работы"
+						id="medalSearchId" type="text" value={getHumanName} onChange={onLabelChange}  onKeyDown={_handleKeyDown} />
+					<Div>
+						<Button size="l" title="Искать на сайте: Медаль ЗА ОБОРОНУ ЛЕНИИНГРАДА" onClick={search}>
+							Искать
+						</Button>
+
+					</Div>
+					<Div style={{ paddingTop: 60, paddingBottom: 60, color: 'gray' }}>
+						В базу внесены данные на <b>167&nbsp;785</b> персоналий. Работа продолжается
+					</Div>
+
+				</Div>
+			</Group>
+		</Panel>
+	)
 };
 
 export default Home;
