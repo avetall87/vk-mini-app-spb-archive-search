74bdd97f35bcf1a608e8640cb2423c6be8b8724f
avorontsov <vorontsov@iac.spb.ru>
2020-11-02 11:30:19 +0300

    Add support banner

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -10,16 +10,30 @@ import Search from '@vkontakte/vkui/dist/components/Search/Search';
 
 import './Home.css';
 
+import logo from '../img/medal_32_32.png';
+import priznanie from '../img/narodnoe-priznanie-logotip_150.png';
+
 
 const Home = ({id, go, fetchedUser}) => {
 
 	const [getName, setName] = useState('');
 	const [getHumanName, setHumanName] = useState('');
+	const SUPPORT_PROJECT_URL = "http://2020.prof-it.d-russia.ru/medal-za-oborony-leningrada";
+	const IAC_URL = "https://iac.spb.ru";
+
 
 	useEffect(() => {
 		//setName('');
 		reset();
 	}, []);
+
+	const supportProject = async () => {
+		window.open(SUPPORT_PROJECT_URL);
+	}
+
+	const gotoDeveloperSite = async () => {
+		window.open(IAC_URL);
+	}
 
 	const search = async () => {
 		window.open(`https://medal.spbarchives.ru/search?query=${getHumanName}&advancedSearch=false&from=vk`);
@@ -45,14 +59,14 @@ const Home = ({id, go, fetchedUser}) => {
 
 				<PanelHeaderContent
 					// status="В базу внесены данные на 167 785 персоналий. Работа продолжается"
-					before={<Avatar size={36} src="https://spbarchives.ru/documents/10157/1507999/medal_150_150.png" />}
+					before={<Avatar size={36} src={logo} />}
 				>
 					<span class="PageHeaderContent"> Медаль «За оборону Ленинграда»</span>
 				</PanelHeaderContent>
 				{/*Медаль «За оборону Ленинграда»*/}
 			</PanelHeader>
 			<Group>
-				<Div>
+				<Div style={{ paddingTop: 0}}>
 					<h3 align="left" style={{ paddingLeft: 16, fontWeight: 500}} >Поиск награжденных медалью</h3>
 					<Search
 						placeholder="ФИО, год рождения, место работы"
@@ -63,8 +77,17 @@ const Home = ({id, go, fetchedUser}) => {
 						</Button>
 
 					</Div>
-					<Div style={{ paddingTop: 60, paddingBottom: 60, color: 'gray' }}>
+					<Div style={{ paddingTop: 20, color: 'gray' }}>
 						В базу внесены данные на <b>167&nbsp;785</b> персоналий. Работа продолжается
+					</Div>
+
+					<Div style={{ paddingTop: 30, vAlign: 'top'}}>
+						Вы можете <a onClick={supportProject} className="link">поддержать проект</a> в конкурсе «Народное признание».
+						<img src={priznanie} className="priznanieLogo" onClick={supportProject}/>
+					</Div>
+
+					<Div style={{ paddingTop: 0, paddingRight: 30, color: 'gray', fontSize: 14, textAlign: 'right'}}>
+						Разработано <a onClick={gotoDeveloperSite} className="link">СПБ ГУП «СПБ ИАЦ»</a>.
 					</Div>
 
 				</Div>
