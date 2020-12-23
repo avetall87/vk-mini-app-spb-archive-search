9a90c5b4c435fd9b94a4d83b88f813291b9a42d2
Andrey Mironov <mironov.a@iac.spb.ru>
2020-12-23 12:36:40 +0300

    VMA-27

--- src/panels/notification/NotificationApiService.js
+++ src/panels/notification/NotificationApiService.js
@@ -1,41 +1,20 @@
+import {RemoteAPI} from '../../utils/RemoteAPI';
+
 export class NotificationApiService {
 
     constructor() {
 
     }
 
-    async subscribeToNotification (userId, searchQuery) {
+    async subscribeToNotification(userId, searchQuery) {
 
-        let notification = {
+        const notification = {
             userId: userId,
-            searchQuery: searchQuery,
-            requestId: 123
+            searchQuery: searchQuery
         }
 
-        const url = 'https://special.spbarchives.ru/api/v1/vk/notification/add'
-        const response = await fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            // mode: 'same-origin', // no-cors, *cors, same-origin
-            // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            // credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *client
-            body: notification // body data type must match "Content-Type" header
-        });
+        const api = new RemoteAPI();
 
-        if (response.ok) { // если HTTP-статус в диапазоне 200-299
-                           // получаем тело ответа (см. про этот метод ниже)
-            return await response.json();
-        } else {
-            alert("Ошибка HTTP: " + response.status);
-            return null;
-        }
-
-        // console.log(JSON.stringify(notification));
-
-        // return "success";
+        return await api.post('/api/v1/vk/notification/add', notification);
     }
 }
